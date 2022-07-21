6b371764817cb74e46908d425531dd2603f88c6a
Liam Nichols <liam.nichols.ln@gmail.com>
2022-07-21 17:38:34 +0200

    Improve error handling and output a summary message  (#2)
    * Remove -e flag to prevent script from failing on error
    
    * Log response from the server to help with debugging
    
    * Set SNOWPLOW_MICRO_COLLECTOR_SUMMARY with a summary of the step
    
    * Add fail_for_bad_reset flag
    
    * Rename fail_for_bad_reset to fail_on_error and add finish helper
    
    * Tweak summary wording upon success

--- step.sh
+++ step.sh
@@ -1,18 +1,41 @@
 #!/bin/bash
-set -e
+
+# Helper for finishing and exiting.
+#   $0 - A summary message to be printed and exported
+#   $1 - The script exit code. Defaults to 0, will be overwritten if $fail_on_error is set to 'no'
+finish () {
+  summary="$1"
+  exit_code="$2"
+
+  # Set the summary as an output for use in future steps
+  envman add --key SNOWPLOW_MICRO_COLLECTOR_SUMMARY --value "$summary"
+
+  # Downgrade any errors if fail_on_error is turned off
+  if [ "$fail_on_error" = "no" ]
+  then
+    unset exit_code
+  fi
+
+  # Finish and exit with the appropriate code
+  echo "$summary"
+  exit "${exit_code:-0}"
+}
 
 # Reset the Snowplow collector state
-echo "Reset the state of the collector at $collector_url..."
-result=$(curl --silent "$collector_url/micro/reset")
+echo "Resetting the state of the collector at $collector_url..."
+result=$(curl "$collector_url/micro/reset")
+echo "Response from the server: '$result'"
 
 # Parse the results
 total=$(jq -r '.total' <<< "$result")
 
-# Check if there are any active events 
-if [[ $total -gt 0 ]]
+# Check the result and produce a summary, mark the step as failed if required
+if [[ -z "$total" ]]
 then
-  echo "The Snowplow micro failed to reset."
-  exit 1
+  finish "Unable to retrieve status from the collector" 1
+elif [[ $total -gt 0 ]]
+then
+  finish "The collector failed to reset" 1
 else
-  echo "Collector reset successfully"
+  finish "The collector was reset, waiting for events" 0
 fi
