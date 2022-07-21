623f5db43c997e51a1747ea8b633a1a778792fc4
Liam Nichols <liam.nichols.ln@gmail.com>
2022-07-21 14:01:44 +0200

    Remove -e flag to prevent script from failing on error

--- step.sh
+++ step.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Reset the Snowplow collector state
 echo "Reset the state of the collector at $collector_url..."
@@ -8,8 +7,8 @@ result=$(curl --silent "$collector_url/micro/reset")
 # Parse the results
 total=$(jq -r '.total' <<< "$result")
 
-# Check if there are any active events 
-if [[ $total -gt 0 ]]
+# Check the result and produce a summary, mark the step as failed if required
+if [[ -z $total ]] || [[ $total -gt 0 ]]
 then
   echo "The Snowplow micro failed to reset."
   exit 1
